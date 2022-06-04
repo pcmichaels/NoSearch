3fc0d31c3f2ae3992aef18ffc70a181e2a437d87
Paul Michaels <pcmichaels@live.com>
2022-06-04 12:37:46 +0100

    Add unit test

--- test/NoSearch.Tests/SearchTests.cs
+++ test/NoSearch.Tests/SearchTests.cs
@@ -1,7 +1,34 @@
-﻿namespace NoSearch.Tests
+﻿using Moq;
+using NoSearch.App.Search;
+using NoSearch.Data;
+using NoSearch.Models;
+using Xunit;
+
+namespace NoSearch.Tests
 {
     public class SearchTests
     {
+        [Fact]
+        public void SearchResources_CorrectResultsReturned()
+        {
+            // Arrange
+            var resourceDataAccess = new Mock<IResourceDataAccess>();
+            resourceDataAccess.Setup(a => a.GetAllResources()).Returns(
+                new List<Resource>()
+                {
+                    new Resource() { Name = "abc", Description = "abc" },
+                    new Resource() { Name = "test", Description = "abc" },
+                    new Resource() { Name = "abc", Description = "testing" },
+                    new Resource() { Name = "aaa", Description = "bbb" }
+                });
+            var searchService = new SearchService(resourceDataAccess.Object);
 
+            // Act
+            var result = searchService.SearchResources("test", false);
+
+            // Assert
+            Assert.Equal(2, result.Count());
+            Assert.Contains(result, a => a.Name == "abc");
+        }
     }
 }
